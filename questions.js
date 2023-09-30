11bda9f28823a0934f42c3645ce9505892fc545b
unknown <anhehandal@gmail.com>
2023-09-30 17:00:27 -0600

    new project

---
+++ questions.js
@@ -0,0 +1,131 @@
+export const easyNorseMythologyQuestions = [
+    {
+      question: "Who is the chief god in Norse mythology?",
+      answer: "Odin",
+    },
+    {
+      question: "What is the name of the hammer wielded by Thor?",
+      answer: "Mjolnir",
+    },
+    {
+      question: "Which mythical tree connects the nine realms in Norse mythology?",
+      answer: "Yggdrasil",
+    },
+    {
+      question: "Who is the trickster god in Norse mythology?",
+      answer: "Loki",
+    },
+    {
+      question: "What is the name of the golden hall in Asgard, home to some of the gods?",
+      answer: "Valhalla",
+    },
+    {
+      question: "Which monstrous serpent is destined to fight Thor during Ragnarök?",
+      answer: "Jormungandr",
+    },
+    {
+      question: "What are the female spirits of fate who control human destinies in Norse mythology called?",
+      answer: "Norns",
+    },
+    {
+      question: "Which goddess is associated with love and beauty in Norse mythology?",
+      answer: "Freyja",
+    },
+    {
+      question: "What is the name of the legendary wolf that will play a role in the events of Ragnarök?",
+      answer: "Fenrir",
+    },
+    {
+      question: "Who is the guardian of the Bifrost, the rainbow bridge that connects Asgard to the other realms?",
+      answer: "Heimdall",
+    },
+  ];
+
+
+export  const mediumNorseMythologyQuestions = [
+    {
+      question: "What is the name of Odin's magical, eight-legged horse?",
+      answer: "Sleipnir",
+    },
+    {
+      question: "Which giant is destined to blow the horn at Ragnarök, signaling the end of the world?",
+      answer: "Heimdall",
+    },
+    {
+      question: "Which two animals are said to pull the chariot of the moon in Norse mythology?",
+      answer: "Hati and Skoll",
+    },
+    {
+      question: "What is the name of the rainbow bridge that connects Midgard (Earth) to Asgard?",
+      answer: "Bifrost",
+    },
+    {
+      question: "Who is the father of the thunder god Thor?",
+      answer: "Odin",
+    },
+    {
+      question: "Which dwarves created Thor's hammer, Mjölnir?",
+      answer: "Brokkr and Sindri",
+    },
+    {
+      question: "What is the name of the magical mead of poetry in Norse mythology?",
+      answer: "Mead of Poetry",
+    },
+    {
+      question: "Which goddess is associated with the underworld and is the daughter of Loki?",
+      answer: "Hel",
+    },
+    {
+      question: "What is the name of the world inhabited by the light elves in Norse mythology?",
+      answer: "Alfheim",
+    },
+    {
+      question: "Who is the giantess that is married to the sea god Ægir and hosts feasts for the gods?",
+      answer: "Ran",
+    },
+  ];
+
+
+export  const hardNorseMythologyQuestions = [
+    {
+      question: "What is the name of the ship made from the fingernails of the dead that will carry the giants during Ragnarök?",
+      answer: "Naglfar",
+    },
+    {
+      question: "Who is the father of the wolf Fenrir, the serpent Jörmungandr, and Hel, the ruler of the dead?",
+      answer: "Loki",
+    },
+    {
+      question: "What is the name of the rooster that will crow to signal the beginning of Ragnarök?",
+      answer: "Gullinkambi",
+    },
+    {
+      question: "Which dwarf turned into a dragon after being cursed by the gods for his greed?",
+      answer: "Fafnir",
+    },
+    {
+      question: "What is the name of the boar that is said to be a harbinger of Ragnarök?",
+      answer: "Gullinbursti",
+    },
+    {
+      question: "Which two beings will survive Ragnarök and repopulate the world?",
+      answer: "Lif and Lifþrasir",
+    },
+    {
+      question: "Who is the giantess who tried to marry Thor but was ultimately tricked by him?",
+      answer: "Skadi",
+    },
+    {
+      question: "What is the name of the great wolf that will devour the sun during Ragnarök?",
+      answer: "Skoll",
+    },
+    {
+      question: "What is the name of the sword that Odin wields and which can slay the wolf Fenrir during Ragnarök?",
+      answer: "Gungnir",
+    },
+    {
+      question: "Which god is known as the protector of justice and law in Norse mythology?",
+      answer: "Tyr",
+    },
+  ];
+
